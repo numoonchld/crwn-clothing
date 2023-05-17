d502b84fcb94167aa528ac333199d89325be2198
numoonchld <raghavendra.saralaya@gmail.com>
2023-05-17 18:10:06 +0530

    home page links working

--- src/components/directory-item/directory-item.component.js
+++ src/components/directory-item/directory-item.component.js
@@ -1,9 +1,14 @@
+import { useNavigate } from "react-router-dom";
 import { BackgroundImage, Body, DirectoryItemContainer } from "./directory-item.styles.js";
 
 const DirectoryItem = ({ category }) => {
-  const { title, imageUrl } = category;
+  const { title, imageUrl, route } = category;
+  const navigate = useNavigate()
+
+  const onNavigateHandler = () => navigate(route)
+
   return (
-    <DirectoryItemContainer>
+    <DirectoryItemContainer onClick={onNavigateHandler}>
       <BackgroundImage
         imgurl={imageUrl}
       />
