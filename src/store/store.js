50d7a2ffb5c17d53f97e22226db7bc403b61e750
numoonchld <raghavendra.saralaya@gmail.com>
2023-05-29 21:27:32 +0530

    non-production only logger

--- src/store/store.js
+++ src/store/store.js
@@ -5,18 +5,19 @@ import storage from 'redux-persist/lib/storage'
 
 import { rootReducer } from './root-reducer'
 
-
-const middleWares = [process.env.NODE_ENV !== 'production' && logger].filter(Boolean)
-
 const persistConfig = {
     key: 'root',
     storage,
     blacklist: ['user']
 }
 
+const middleWares = [process.env.NODE_ENV !== 'production' && logger].filter(Boolean)
+
 const persistedReducer = persistReducer(persistConfig, rootReducer)
 
-const composedEnhancers = compose(applyMiddleware(...middleWares))
+const composeEnhancer = (process.env.NODE_ENV !== 'production' && window && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose
+
+const composedEnhancers = composeEnhancer(applyMiddleware(...middleWares))
 
 export const store = createStore(persistedReducer, undefined, composedEnhancers)
 
