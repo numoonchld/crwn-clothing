e03c39cd80fb124eed4c8fd6e906afad4daddc47
numoonchld <raghavendra.saralaya@gmail.com>
2023-06-27 15:34:18 +0530

    categories redux toolkit adaptation

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,37 +1,32 @@
-import React, { useEffect } from 'react'
+import React, { useEffect } from "react";
 
-import './shop.styles.scss'
-import { Route, Routes } from 'react-router-dom'
-import CategoriesPreview from '../categories-preview/categories-preview.component'
-import Category from '../category/category.component'
-import { getCategoriesAndDocuments } from '../../utils/firebase.utils'
+import "./shop.styles.scss";
+import { Route, Routes } from "react-router-dom";
+import CategoriesPreview from "../categories-preview/categories-preview.component";
+import Category from "../category/category.component";
+import { getCategoriesAndDocuments } from "../../utils/firebase.utils";
 
-import { setCategories } from '../../store/categories/categories.action'
-import { useDispatch } from 'react-redux'
+import { setCategories } from "../../store/categories/categories.slice";
+import { useDispatch } from "react-redux";
 
 const Shop = () => {
+  const dispatch = useDispatch();
 
-    const dispatch = useDispatch()
+  useEffect(() => {
+    const getCategoriesMap = async () => {
+      const categories = await getCategoriesAndDocuments();
+      dispatch(setCategories(categories));
+    };
 
-    useEffect(() => {
+    getCategoriesMap();
+  }, []);
 
-        const getCategoriesMap = async () => {
-            const categories = await getCategoriesAndDocuments()
-            dispatch(setCategories(categories))
-        }
+  return (
+    <Routes>
+      <Route index element={<CategoriesPreview />} />
+      <Route path=":category" element={<Category />} />
+    </Routes>
+  );
+};
 
-        getCategoriesMap()
-
-
-    }, [])
-
-
-    return (
-        <Routes>
-            <Route index element={<CategoriesPreview />} />
-            <Route path=':category' element={<Category />} />
-        </Routes>
-    )
-}
-
-export default Shop
+export default Shop;
--- src/store/root-reducer.js
+++ src/store/root-reducer.js
@@ -1,7 +1,7 @@
 import { combineReducers } from "@reduxjs/toolkit";
 
 import { userReducer } from "./user/user.slice";
-import { categoriesReducer } from "./categories/categories.reducer";
+import { categoriesReducer } from "./categories/categories.slice";
 import { cartReducer } from "./cart/cart.reducer";
 
 export const rootReducer = combineReducers({
