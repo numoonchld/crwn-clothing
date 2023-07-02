c9986d4c832be5cdd2ec6fc27d620a807f1fc417
numoonchld <raghavendra.saralaya@gmail.com>
2023-07-02 17:10:52 +0530

    progress save

--- src/index.js
+++ src/index.js
@@ -8,6 +8,8 @@ import { PersistGate } from "redux-persist/integration/react";
 
 import { Provider } from "react-redux";
 import { persistor, store } from "./store/store";
+import { Elements } from "@stripe/react-stripe-js";
+import { stripePromise } from "./utils/stripe/stripe.utils";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -15,7 +17,9 @@ root.render(
     <Provider store={store}>
       {/* <PersistGate loading={null} persistor={persistor}> */}
       <BrowserRouter>
-        <App />
+        <Elements stripe={stripePromise}>
+          <App />
+        </Elements>
       </BrowserRouter>
       {/* </PersistGate> */}
     </Provider>
--- src/routes/checkout/checkout.component.js
+++ src/routes/checkout/checkout.component.js
@@ -1,32 +1,46 @@
-import CheckoutItem from '../../components/checkout-item/checkout-item.component'
-import { selectCartItems, selectCartTotal } from '../../store/cart/cart.selector'
-import './checkout.styles.scss'
+import CheckoutItem from "../../components/checkout-item/checkout-item.component";
+import {
+  selectCartItems,
+  selectCartTotal,
+} from "../../store/cart/cart.selector";
+import "./checkout.styles.scss";
 
-import React from 'react'
-import { useSelector } from 'react-redux'
+import React from "react";
+import { useSelector } from "react-redux";
+import PaymentForm from "../../components/payment-form/payment-form.component";
 
 const Checkout = () => {
+  // const { isCartOpen, setIsCartOpen, cartItems, cartTotal } = useContext(CartContext)
+  const cartItems = useSelector(selectCartItems);
+  const cartTotal = useSelector(selectCartTotal);
 
-    // const { isCartOpen, setIsCartOpen, cartItems, cartTotal } = useContext(CartContext)
-    const cartItems = useSelector(selectCartItems)
-    const cartTotal = useSelector(selectCartTotal)
-
-    return (
-        <div class='checkout-container'>
-            <div className='checkout-header'>
-                <div className='header-block'><span>Product</span></div>
-                <div className='header-block'><span>Description</span></div>
-                <div className='header-block'><span>Quantity</span></div>
-                <div className='header-block'><span>Price</span></div>
-                <div className='header-block'><span>Remove</span></div>
-            </div>
-
-            {
-                cartItems.map(cartItem => <CheckoutItem key={cartItem.id} cartItem={cartItem} />)
-            }
-            <span className='total'>Total: ${cartTotal}</span>
+  return (
+    <div className="checkout-container">
+      <div className="checkout-header">
+        <div className="header-block">
+          <span>Product</span>
         </div>
-    )
-}
+        <div className="header-block">
+          <span>Description</span>
+        </div>
+        <div className="header-block">
+          <span>Quantity</span>
+        </div>
+        <div className="header-block">
+          <span>Price</span>
+        </div>
+        <div className="header-block">
+          <span>Remove</span>
+        </div>
+      </div>
 
-export default Checkout
+      {cartItems.map((cartItem) => (
+        <CheckoutItem key={cartItem.id} cartItem={cartItem} />
+      ))}
+      <span className="total">Total: ${cartTotal}</span>
+      <PaymentForm />
+    </div>
+  );
+};
+
+export default Checkout;
